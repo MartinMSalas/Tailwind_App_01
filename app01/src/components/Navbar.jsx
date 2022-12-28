871b3306d8de28fa4d33fc29e65b378fd15a6bfb
MartinMSalas <ingmartinmsalas@gmail.com>
2022-12-28 09:25:58 -0300

    navbar useState

--- app01/src/components/Navbar.jsx
+++ app01/src/components/Navbar.jsx
@@ -1,7 +1,9 @@
-import React  from 'react';
+import React, {useState}  from 'react';
 import {Bars3Icon, } from "@heroicons/react/24/outline"
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  
   return ( 
     <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadlow-lg'>
       <div className='px-2 flex justify-between item-center w-full h-full'>
@@ -31,6 +33,11 @@ const Navbar = () => {
         <li className='border-b-2 border-zinc-300 w-full'>Platforms</li>
         <li className='border-b-2 border-zinc-300 w-full'>Platforms</li>
         <li className='border-b-2 border-zinc-300 w-full'>Pricing</li>
+        <div className='flex flex-col my-4'>
+
+          <button className='bg-transparent text-indigo-600 px-8 py-3 mb-4'>Sign In</button>
+          <button className='px-8 py-3'>Sign Up</button>
+        </div>
       </ul>
     </div>
    )
