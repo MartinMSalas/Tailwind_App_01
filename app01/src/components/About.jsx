8716fcd4f37021e53747cdad2fd08e7a92dad440
MartinMSalas <ingmartinmsalas@gmail.com>
2023-01-09 11:16:29 -0300

    Support

--- app01/src/components/About.jsx
+++ app01/src/components/About.jsx
@@ -10,17 +10,17 @@ const About = () => {
           <p className="text-3xl py-6 text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error expedita quisquam quibusdam vero! Incidunt modi dolore error itaque vero quisquam minima reiciendis quod distinctio, libero ipsum, numquam accusantium quos. Quo!</p>
         </div>
         <div className="grid md:grid-cols-3 gap-1 px-2 text-center">
-          <div>
-            <p>100%</p>
-            <p>Completion</p>
+          <div className="border py-8 rounded-xl shadow-xl">
+            <p className="text-6xl font-bold text-indigo-600">100%</p>
+            <p className="text-gray-400 mt-2">Completion</p>
           </div>
-          <div>
-            <p>24/7</p>
-            <p>Delivery</p>
+          <div className="border py-8 rounded-xl shadow-xl">
+            <p className="text-6xl font-bold text-indigo-600">24/7</p>
+            <p className="text-gray-400 mt-2">Delivery</p>
           </div>
-          <div>
-            <p>100k</p>
-            <p>Transactions</p>
+          <div className="border py-8 rounded-xl shadow-xl">
+            <p className="text-6xl font-bold text-indigo-600">100k</p>
+            <p className="text-gray-400 mt-2">Transactions</p>
           </div>
         </div>
       </div>
--- app01/src/components/App.js
+++ app01/src/components/App.js
@@ -1,12 +1,14 @@
 import About from "./About";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
+import Support from "./Support";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Hero></Hero>
+      <Support></Support>
       <About></About>
     </>
   )
